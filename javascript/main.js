9ed5e148da42bb00a9e829d7de9d487bc28061c7
Dineshkhang <dineshkhang143@gmail.com>
2025-03-25 16:22:41 +0545

    started js learnig

---
+++ javascript/main.js
@@ -0,0 +1,27 @@
+// console.log("Apple");
+// let a=5
+//  a=a+1;
+// console.log(a)
+// const b =5
+// // b = b+1;
+// console.log(b)
+let a=10
+let b=20
+let c=a+b
+console.log(c)
+let d=10
+let e=20
+let f=e-d
+console.log(f)
+let g=10
+let h=20
+let i=h*g
+console.log(i)
+let j=10
+let k=20
+let l=j/k
+console.log(l)
+
+console.log(10==10);
+
+
