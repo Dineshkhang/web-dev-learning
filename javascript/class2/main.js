fa3865ea38ae8ba98e170c5206f0bdf55a7217c9
Dineshkhang <dineshkhang143@gmail.com>
2025-03-26 21:36:58 +0545

    simple program in js

---
+++ javascript/class2/main.js
@@ -0,0 +1,66 @@
+// let a =7;
+// if(a==9){
+//     console.log("no");
+    
+// }
+// else{
+//     console.log("yes");
+    
+// }
+
+
+// let a=0;
+// if (a==9) {
+//     console.log("yes");
+    
+// }
+// else if (a==8) {
+// console.log("NO");
+
+// }
+// else if (a==7) {
+//     console.log("invalid value");
+    
+// }
+// else{
+//     console.log("null");
+    
+// }
+
+// let a= 40;
+// if (a%2==0) {
+//     console.log("a is a even Number ");
+    
+// } else {
+//     console.log("a is a odd");
+    
+// }
+
+let a=12;
+let b=24;
+if (a>b) {
+   console.log("A is greater");
+    
+} else {
+    console.log("B is greater");
+    
+}
+
+let z="z"
+switch (z) {
+    case "z":
+        console.log("zebra");
+        break;
+case "y":
+    console.log("yak")
+break
+case "x":
+    console.log("x-ray")
+break
+case "w":
+    console.log("watch")
+break
+    default:
+    console.log("Not valid");
+    
+}
