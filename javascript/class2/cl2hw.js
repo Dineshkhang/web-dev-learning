fecf0514f940c57190712f831a12b6f06889a984
Dineshkhang <dineshkhang143@gmail.com>
2025-03-27 16:17:08 +0545

    looping learning

---
+++ javascript/class2/cl2hw.js
@@ -0,0 +1,66 @@
+let a=3;
+let b=6;
+if (a<b) {
+  console.log("A is smallest");
+    
+} else {
+   console.log("B is smallest");
+    
+}
+
+let num = 10;
+switch (num) {
+    case 1:
+        console.log("Baishak");
+        
+        break;
+    case 2:
+        console.log("jestha");
+        
+        break;
+    case 3:
+        console.log("ashar");
+        
+        break;
+    case 4:
+        console.log("sharwan");
+        
+        break;
+    case 5:
+        console.log("Bardha");
+        
+        break;
+    case 6:
+        console.log("ashwin");
+        
+        break;
+    case 7:
+        console.log("kartik");
+        
+        break;
+    case 8:
+        console.log("mansir");
+        
+        break;
+    case 9:
+        console.log("posh");
+        
+        break;
+    case 10:
+        console.log("magh");
+        
+        break;
+    case 11:
+        console.log("falgun");
+        
+        break;
+    case 12:
+        console.log("chatira");
+        
+        break;
+
+    default:
+        console.log("invalid number");
+        
+        break;
+}
---
+++ javascript/class3/main.js
@@ -0,0 +1,50 @@
+ // for loop
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+    
+}
+
+ // while loop
+let x=1;
+while (x<10) {
+    console.log(x);
+    x++
+}
+
+// do while
+y=9
+do {
+    console.log(y);
+    y++;
+} while (y<10);
+
+// using do while loops to print even Number
+let a=1;
+do{
+if (a%2==0) {
+    console.log(a, "is even number");
+}
+a++;
+} while (a<=20); 
+  
+
+// table 
+
+for (let j = 1; j <=10; j++){
+    console.log("9x",j,"=",j*9);
+    
+}
+
+// // function
+function d() {
+    console.log("Hello world");
+    
+}
+d();
+
+// eg2
+function e(d) {
+    console.log("Name is",d);
+    
+}
+e("Dinesh");
