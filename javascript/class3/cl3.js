cc9f686163ac91f78cf33ad4ca77a383d87aa39a
Dineshkhang <dineshkhang143@gmail.com>
2025-03-28 16:05:11 +0545

    js function

---
+++ javascript/class3/cl3.js
@@ -0,0 +1,24 @@
+for (let a = 1; a < 11; a++) {
+    console.log ("4x",a,"=",a*4);
+}
+for (let j = 1; j <=10; j++) {
+    console.log("5x",j,"=",j*5);
+    
+}
+for (let b = 1; b <=10; b++){
+    console.log("6x",b,"=",b*6); 
+}
+// let number = 4;
+let i = 1;
+
+while (i <= 10) {
+    console.log("4X",i,"=",4*i);
+    i++;
+}
+
+let c=0;
+do {
+    c++
+    console.log("5x",c,"=",5*c);
+    
+} while (c<=9);
---
+++ javascript/class4/app.js
@@ -0,0 +1,33 @@
+// function c() {
+//     let a=4;
+//     console.log(a);
+    
+// }
+// c();
+
+
+// let a=0;
+// function b() {
+//     a=4
+//     console.log(a);
+    
+// }
+// console.log(a);
+// b();
+// console.log(a);
+
+// // IIFE immediately invoked Functional expression
+// let q=6;
+// (function p() {
+//     console.log(q);
+//     q=4;
+//     console.log(q);
+// })()
+
+function sum (e,b) {
+  let s=e+b;
+  return s; 
+
+}
+let e=sum(4,5);
+console.log(e);
