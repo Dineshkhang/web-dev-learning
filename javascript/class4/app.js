d45379d2905a1482a467c60fcc8f13b0550246b7
Dineshkhang <dineshkhang143@gmail.com>
2025-03-30 07:33:21 +0545

    function learning

--- javascript/class4/app.js
+++ javascript/class4/app.js
@@ -24,10 +24,36 @@
 //     console.log(q);
 // })()
 
-function sum (e,b) {
-  let s=e+b;
-  return s; 
+// function sum (e,b) {
+//   let s=e+b;
+//   return s; 
 
-}
-let e=sum(4,5);
-console.log(e);
+// }
+// let e=sum(4,5);
+// console.log(e);
+
+
+// new function
+// const sum=( g,h) => g+h;{
+//   return sum
+// }
+// console.log(sum(6,8));
+// add 
+const add = (a, b) => a + b;
+
+console.log(add(5, 3)); 
+
+// sub
+const sub = (a, b) => a - b;
+console.log(sub(5, 3)); 
+
+// mul
+const mul = (a, b) => a * b;
+console.log(mul(5, 3)); 
+
+// div 
+const div = (a, b) => a / b;
+
+console.log(div(9, 3)); 
+
+
