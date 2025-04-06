ac4de9fc0cf587303b7fe8ae90e564550693ad15
Dineshkhang <dineshkhang143@gmail.com>
2025-04-06 12:56:50 +0545

    video 2 learning of js by harry

---
+++ yt-learning/javascript/video2/script.js
@@ -0,0 +1,32 @@
+console.log("Here is the learning of js video2")
+let  a = 5;
+let b = 6;
+let c = "Dinesh"
+console.log(a + b +2); // here js automatically detect 2 as a number and add it.
+console.log(typeof a, typeof b, typeof c );
+
+// const a1 = 4;
+// a1 = a1+1; not allowed to change const value
+
+
+let x = "haa"
+let y = "23"
+let z = 3.55;
+const p =true;
+let q = undefined;
+let r = null; // type of is object
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let o = {
+"name" : "Dinesh",
+"job code": 5544,
+"is handsome": true
+}
+console.log(o);
+o.salary = "100crores"
+console.log(o);
+o.salary = "1000crores";
+console.log(o);
+
