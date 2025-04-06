4ba99f625d528052204774e7b0a23463ff728626
Dineshkhang <dineshkhang143@gmail.com>
2025-04-06 12:26:08 +0545

    st js

---
+++ yt-learning/javascript/video1/app.js
@@ -0,0 +1,12 @@
+// alert(" Hello world");
+var a =prompt("enter your number")
+var isTrue = confirm("are you sure")
+if (isTrue) {
+    console.log("sucessfully run the first program");
+}
+else{
+    console.log("don't run");
+    
+}
+document.little = "I am changing tittle"
+// document.body.style.backgroundColor = "red"
---
+++ yt-learning/javascript/video1/script.js
@@ -0,0 +1,4 @@
+console.log("helloWorld");
+console.log("code is running..");
+console.log("code is also running..");
+console.log("code is looking like a wow");
