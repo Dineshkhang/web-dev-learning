b8fbb9f3684f634c3a848dd9218fda027232888c
Dineshkhang <dineshkhang143@gmail.com>
2025-04-07 22:43:39 +0545

    api

---
+++ javascript/class10/script.js
@@ -0,0 +1,23 @@
+const fn = async () => {
+  const response = await fetch(
+    "https://fakestoreapi.com/products"
+  );
+  const data = await response.json();
+  return data;
+};
+
+// let a = document.querySelector(".jokes");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const data = await fn();
+  console.log("data", data);
+});
+data.innerHTML``;
+//   a.innerHTML = `
+//   <div>
+//   <h1>${data.setup}</h1>
+// <p>${data.punchline}</p>
+
+//   </div>
+//   `;
+// });
---
+++ javascript/class10/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+  a.innerHTML = `
+  <div>
+  <h1>${data.setup}</h1>
+<p>${data.punchline}</p>
+
+  </div>
+  `;
